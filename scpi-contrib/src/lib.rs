04ecb8aa21dee799d42b12c4666843461dc8ae1b
Adam Gausmann <adam@gaussian.dev>
2024-05-26 14:35:25 -0500

    scpi-contrib: Set no_std attribute when std is not enabled

--- scpi-contrib/src/lib.rs
+++ scpi-contrib/src/lib.rs
@@ -19,6 +19,8 @@
 //! 1. [IEEE488.2](https://standards.ieee.org/ieee/488.2/718/)
 //! 2. [SCPI-99](https://www.ivifoundation.org/docs/scpi-99.pdf)
 
+#[cfg_attr(not(feature = "std"), no_std)]
+
 #[cfg(all(feature = "alloc", not(feature = "std")))]
 extern crate alloc;
 #[cfg(feature = "std")]
